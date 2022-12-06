406e44c9835f1052426d2a89020ad439f108badf
ksg1766 <star1766@daum.net>
2022-12-07 06:19:38 +0900

    Update Assignment.sol
    불필요한 주석 및 코드 수정

--- src/Assignment.sol
+++ src/Assignment.sol
@@ -10,14 +10,14 @@ contract BikeData is Ownable {
     //등록된 자전거의 이름을 저장하는 리스트(맵핑은 length를 통해 for문을 돌릴 수 없기 때문에)
     string[] private BikeNameArray;
 
-    //지저분한 맵핑을 구조체로 정리하는 중...
+    //자전거의 정보 : 가격, 이미지, 판매자의 주소를 저장
     struct BikeStruct {
         uint256 BikePrice;
         bytes BikeImage;
         address BikeSeller;
-        //uint256 BikeFee;
     }
 
+    //자전거의 이름과 정보를 맵핑
     mapping(string => BikeStruct) private BikeInfo;
 
     //자전거 구매 시간(이름:시간)
@@ -27,14 +27,17 @@ contract BikeData is Ownable {
 
     //자전거 이름과 가격 출력을 위한 이벤트(일단 이름, 가격, 이미지 소스, 판매자 이름 출력)
     event ShowBikeInfo(string name_of_bike, uint256 price_of_bike, bytes source_of_image, address name_of_seller);
+    
     //event ShowBikeInfo(string name_of_bike, BikeStruct _bikeInfo);
+    /*
     modifier onlyBefore(uint _time) {
         require(block.timestamp < _time, "Function called too late.");
         _;
     }
+    */
 
     //구매확정기한 지났는지 체크
-    function ifTooLate(uint _time) public view returns(bool){
+    function ifTooLate(uint _time) private view returns(bool){
         if(block.timestamp >= _time)
             return true;
         return false;
@@ -72,7 +75,7 @@ contract BikeData is Ownable {
         //BikeFee[_BikeName] = msg.value;
     }
 
-    //자전거 삭제(자전거 구매 확정 시 사용 할 예정) - 필요가 있는지 모르겠음
+    //자전거 삭제(자전거 구매 확정 시 사용 할 예정)
     function delBike(string memory _BikeName) public Owned {
         require(searchBike(_BikeName) < 999, "Check the name");
 
@@ -100,9 +103,7 @@ contract BikeData is Ownable {
         BikeNameArray.pop();
 
         (bool _success,) = address(this).call{value:msg.value}("");
-        (bool __success,) = address(this).call{value:BikeInfo[_BikeName].BikePrice / 100}("");
         require(_success,"Failed to pay");
-        require(__success,"Failed to pay");
 
         //보낸 돈이 너무 많으면 잔액 반송.
         if(getBikePrice(_BikeName) < msg.value){
@@ -125,7 +126,7 @@ contract BikeData is Ownable {
             delete BikeBuyer[_BikeName];
             delete purchasedTime[_BikeName];
         }
-        else { //3일 지났으면 그냥 돈돈 돌려주기. 다만 구매자 확인을 거쳐야 함.
+        else { //3일 지났으면 그냥 돈 돌려주기. 다만 구매자 확인을 거쳐야 함.
             refund(_BikeName);
             BikeNameArray.push(_BikeName);
             //구매자 명단도 삭제제
@@ -135,7 +136,7 @@ contract BikeData is Ownable {
     }
     
     //환불
-    function refund(string memory _BikeName) public payable {
+    function refund(string memory _BikeName) private {
         require(BikeBuyer[_BikeName] == msg.sender, "This account hasn't purchased the product");
         (bool _success,) = msg.sender.call{value: BikeInfo[_BikeName].BikePrice}("");
         require(_success, "Failed to pay");
@@ -159,7 +160,7 @@ contract BikeData is Ownable {
         return address(this).balance;
     }
 
-    function zzzcontractKiller() public Owned {
+    function zzz_contractKiller() public Owned {
         selfdestruct(payable(address(this)));
     }
 }
